1b3f91035b9386082f989f1de987f3649e763e24
alejandro.liebana.7e7 <alejandro.liebana.7e7@itb.cat>
2023-11-29 12:39:50 +0100

    noviembre

---
+++ e4.py
@@ -0,0 +1,23 @@
+'''
+Andrea Alejandro ASIXBc1 29-11-2023
+
+Programa que imprimeix un tauler d’escacs per pantalla.
+Un taulell d’escacs comença amb la casella  Blanca i és de mida 8x8 sempre ;-)
+Fer servir:
+BLANC = "⬜"
+NEGRE = "⬛"
+BLANCO=" "
+NEGRO="██"
+'''
+NEGRO = "  "
+BLANCO = "██"
+
+for i in range(8):
+    for j in range(8):
+        if (i + j) % 2 == 0:
+            print (BLANCO, end="")
+        else:
+            print(NEGRO, end="")
+    print()
+
+
