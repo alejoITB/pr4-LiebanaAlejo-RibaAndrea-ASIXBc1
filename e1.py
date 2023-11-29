05b93fca89deda6ba14ea79f60bb40e39408a6ac
andrea.riba.7e7 <andrea.riba.7e7@itb.cat>
2023-11-29 11:35:04 +0100

    a

--- e1.py
+++ e1.py
@@ -1,1 +1,6 @@
-#ewgwhg
+#
+"""
+e1. Programa que demana a l'usuari la introducció de 10 nombres sencers
+(que també podrien ser 10000000 😱😳😈)
+i ha de mostrar, al final i per pantalla, quants són positius, quants negatius i quants zero.
+"""
